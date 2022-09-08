667434fb89f3376a770ac04b7ec1a2090c04f1c4
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2022-09-08 17:47:52 +0200

    Forget a file

---
+++ src/WebAppTest/Controllers/Models/TestSmtpDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAppTest.Controllers.Models
+{
+    public class TestSmtpDto
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string State { get; set; }
+
+    }
+}
